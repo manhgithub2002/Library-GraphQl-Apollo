bfce4488201abb41f1b88b7d51e102d4780d14fc
Vũ Xuân Mạnh <vuxuanmanh2002@gmail.com>
2023-07-11 09:19:45 +0700

    push all file

---
+++ dist/src/resolver/resolver.js
@@ -0,0 +1,30 @@
+import { authors, books } from "../../data/static.js";
+//resolver
+export const resolvers = {
+  //Query
+  Query: {
+    books: () => books,
+    book(parent, args) {
+      return books.find((book) => book.id.toString() === args.id);
+    },
+    authors: () => authors,
+    author(parent, args) {
+      return authors.find((author) => author.id.toString() === args.id);
+    },
+  },
+  Book: {
+    author: (parent, args) => {
+      return authors.find((author) => author.id === parent.authorId);
+    },
+  },
+  Author: {
+    books: (parent, args) => {
+      return books.filter((book) => book.authorId === parent.id);
+    },
+  },
+  //MUTATION
+  Mutation: {
+    createAuthor: (parent, args) => args,
+    createBook: (parent, args) => args,
+  },
+};
---
+++ src/schema/schema.ts
@@ -0,0 +1,27 @@
+export const typeDefs = `
+  type Book {
+    id: ID
+    name: String
+    genre: String
+    author: Author
+  }
+  
+  type Author {
+    id: ID!
+    name: String
+    age: Int
+    books: [Book]
+  }
+  #ROOT TYPE
+  type Query {
+    books: [Book]
+    book(id: ID!): Book
+    authors: [Author]
+    author(id: ID!): Author
+  }
+
+  type Mutation {
+    createAuthor(id: ID!, name: String, age: Int): Author
+    createBook(id: ID!, name: String, genre: String, authorId: ID!): Book
+  }
+`;
